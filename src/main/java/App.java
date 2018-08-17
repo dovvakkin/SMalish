48db5bc92e92cbd4b8fd55716ab8ffeadc304268
dovvakkin <dovvakkin@gmail.com>
2018-08-17 09:01:08 +0300

    game is on

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public Client client;
+    public EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+        App app = (App) ctx.getBean("app");
+
+        app.logEvent("some event 1");
+    }
+
+    private void logEvent(String msg) {
+        String messege = msg.replaceAll(client.id, client.fullName);
+        eventLogger.logEvent(messege);
+    }
+}
---
+++ src/main/java/Client.java
@@ -0,0 +1,9 @@
+public class Client {
+    String id = new String();
+    String fullName = new String();
+
+    public Client(String id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+}
